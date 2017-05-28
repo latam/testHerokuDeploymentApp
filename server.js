d9ac9ea6d53350adcaa7e67e2ca2a7243e788b18
Mateusz <m_lata@op.pl>
2017-05-28 13:37:13 +0200

    fgfdhdfhdf

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080, function () {
   console.log("RUN on port: " + process.env.PORT || 8080);
 });
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
